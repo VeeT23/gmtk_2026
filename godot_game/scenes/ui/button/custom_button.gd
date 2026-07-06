e292a5b39d1e1a1d6002fc804932245f8377e4a7
VeeT23 <vee239@proton.me>
2026-07-05 20:58:01 -0500

    Placeholder MM

---
+++ godot_game/scenes/ui/button/custom_button.gd
@@ -0,0 +1,32 @@
+extends Button
+
+@onready var hover_stream = $HoverSound
+@onready var click_stream = $ClickSound
+
+var base_text := ""
+var has_mouse := false
+
+signal press_finished()
+
+func _ready() -> void:
+	base_text = text
+	mouse_entered.connect(func():
+		hover_stream.play(0.02)
+		has_mouse = true
+		text = "> "+base_text+" <")
+	
+	mouse_exited.connect(func():
+		has_mouse = false
+		text = base_text)
+	
+	button_up.connect(func():
+		if click_stream.stream == null:
+			press_finished.emit()
+			return
+		click_stream.play()
+		)
+	
+	click_stream.finished.connect(func():
+		if has_mouse:
+			press_finished.emit()
+		)
---
+++ godot_game/scenes/ui/menus/main/main_menu.gd
@@ -0,0 +1,29 @@
+extends CenterContainer
+
+# Menu pages
+@onready var main_page = $Main
+@onready var credits_page = $Credits
+
+# Main page buttons
+
+@onready var start_button = main_page.get_node("Start")
+@onready var settings_button = main_page.get_node("Settings")
+@onready var credits_button = main_page.get_node("Credits")
+@onready var quit_button = main_page.get_node("Quit")
+
+
+
+func select_page(page):
+	for child in get_children():
+		child.hide()
+	page.show()
+
+func _ready() -> void:
+	
+	quit_button.press_finished.connect(func(): get_tree().quit())
+	credits_button.press_finished.connect(select_page.bind(credits_page))
+	
+	for back_button in get_tree().get_nodes_in_group("Back_Button"):
+		back_button.press_finished.connect(select_page.bind(main_page))
+	
+	select_page(main_page)
