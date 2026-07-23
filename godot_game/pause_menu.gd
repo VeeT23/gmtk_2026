a9f6686ea1940d57fc31cfa48d3d3d6de91bb191
VeeT23 <vee239@proton.me>
2026-07-23 03:17:31 -0500

    fixed bug in pause menu that held the cursor hostage

--- godot_game/pause_menu.gd
+++ godot_game/pause_menu.gd
@@ -4,6 +4,8 @@ extends CanvasLayer
 @onready var resume_button: Button = $UI/VBoxContainer/ResumeButton
 @onready var quit_button: Button = $UI/VBoxContainer/QuitButton
 
+var paused = false
+
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	ui.visible = false
@@ -15,8 +17,15 @@ func _unhandled_input(event: InputEvent) -> void:
 		toggle_pause()
 
 func toggle_pause() -> void:
-	get_tree().paused = !get_tree().paused
-	ui.visible = get_tree().paused
+	paused = !paused
+	if paused:
+		get_tree().paused = true
+		ui.visible = true
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		get_tree().paused = false
+		ui.visible = false
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _on_resume_pressed() -> void:
 	toggle_pause()
